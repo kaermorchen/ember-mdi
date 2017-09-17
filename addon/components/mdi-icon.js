ab19caf451998d3045fa9225d58807ba416abaa2
Stanislav Romanov <kaermorchen@gmail.com>
2017-09-17 10:57:24 +0300

    Added extra styles

--- addon/components/mdi-icon.js
+++ addon/components/mdi-icon.js
@@ -1,16 +1,34 @@
 import Ember from 'ember';
 import layout from '../templates/components/mdi-icon';
 
+const { isPresent, computed } = Ember;
+
 const mdiIcon = Ember.Component.extend({
   layout,
   tagName: 'svg',
   classNames: ['mdi-icon'],
-  classNameBindings: ['spin:mdi-icon-spin'],
-  attributeBindings: ['role'],
+  classNameBindings: ['spin:mdi-icon-spin', 'rotateClass', 'flipH:mdi-icon-flip-h', 'flipV:mdi-icon-flip-v'],
+  attributeBindings: ['role', 'size:height', 'size:width', 'viewBox'],
 
+  size: 24,
   role: 'img',
   icon: null,
-  spin: false
+  spin: false,
+  rotate: null,
+  flipH: false,
+  flipV: false,
+
+  viewBox: computed('size', function() {
+    const size = this.get('size');
+
+    return `0 0 ${size} ${size}`;
+  }),
+
+  rotateClass: computed('rotate', function() {
+    const rotate = this.get('rotate');
+
+    return isPresent(rotate) ? `mdi-icon-rotate-${rotate}` : null;
+  })
 });
 
 mdiIcon.reopenClass({
