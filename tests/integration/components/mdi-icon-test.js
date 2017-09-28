a81b0da5b3a241aef082f7c6c8dff7144a4cc91e
Stanislav Romanov <kaermorchen@gmail.com>
2017-09-28 07:37:34 +0300

    Added tests

--- tests/integration/components/mdi-icon-test.js
+++ tests/integration/components/mdi-icon-test.js
@@ -6,19 +6,30 @@ moduleForComponent('mdi-icon', 'Integration | Component | mdi icon', {
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.render(hbs`{{mdi-icon "face"}}`);
+  assert.equal(this.$('svg').html().trim(), '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/assets/icons.svg#face"></use>', 'Icon face is correct');
 
-  this.render(hbs`{{mdi-icon}}`);
+  this.render(hbs`{{mdi-icon icon="bug"}}`);
+  assert.equal(this.$('svg').html().trim(), '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/assets/icons.svg#bug"></use>', 'Icon bug is correct');
 
-  assert.equal(this.$().text().trim(), '');
+  this.render(hbs`{{mdi-icon "bug" size=42}}`);
+  assert.equal(this.$('svg').attr('viewBox'), '0 0 42 42', 'viewBox is correct');
+  assert.equal(this.$('svg').attr('height'), '42', 'height is correct');
+  assert.equal(this.$('svg').attr('width'), '42', 'width is correct');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#mdi-icon}}
-      template block text
-    {{/mdi-icon}}
-  `);
+  this.render(hbs`{{mdi-icon "bug" spin=true}}`);
+  assert.ok(this.$('svg').hasClass('mdi-icon-spin'), 'Spin class');
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  const rotate = 90;
+  this.render(hbs`{{mdi-icon "bug" rotate=90}}`);
+  assert.ok(this.$('svg').hasClass(`mdi-icon-rotate-${rotate}`), 'Rotate class');
+
+  this.render(hbs`{{mdi-icon "bug" flipH=true}}`);
+  assert.ok(this.$('svg').hasClass('mdi-icon-flip-h'), 'Horizontal flip class');
+
+  this.render(hbs`{{mdi-icon "bug" flipV=true}}`);
+  assert.ok(this.$('svg').hasClass('mdi-icon-flip-v'), 'Vertical flip class');
+
+  this.render(hbs`{{mdi-icon "bug" role="icon"}}`);
+  assert.equal(this.$('svg').attr('role'), 'icon', 'Role is correct');
 });
