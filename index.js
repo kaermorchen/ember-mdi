def9599381bac0c8d36b8ae307558eeb5f46eeff
Stanislav Romanov <kaermorchen@gmail.com>
2018-02-04 10:05:29 +0300

    Fixed warnings

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var svgstore = require('broccoli-svgstore');
-var mergeTrees = require('broccoli-merge-trees');
 var Funnel = require('broccoli-funnel');
 var path = require('path');
 
