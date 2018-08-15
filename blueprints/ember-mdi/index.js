27036cc6048a37e970d3c4d1801928f97bd18349
Stanislav Romanov <kaermorchen@gmail.com>
2018-08-15 07:28:30 +0300

    Bump mdi package

--- blueprints/ember-mdi/index.js
+++ blueprints/ember-mdi/index.js
@@ -8,7 +8,7 @@ module.exports = {
 
   afterInstall: function () {
     return this.addPackagesToProject([
-      { name: '@mdi/svg', target: '2.5.94' }
+      { name: '@mdi/svg', target: '2.6.95' }
     ]);
   }
 };
