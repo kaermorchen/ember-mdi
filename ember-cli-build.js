ace15e99574405ce99914e9a1d023f6c1286ad00
Stanislav Romanov <kaermorchen@gmail.com>
2019-06-14 15:15:31 +0300

    Removed fingerprint

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,23 +1,12 @@
 'use strict';
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
-const broccoliAssetRevDefaults = require('broccoli-asset-rev/lib/default-options');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
     'ember-cli-bootstrap-4': {
       js: null
-    },
-
-    fingerprint: {
-      enabled: true,
-      extensions: broccoliAssetRevDefaults.extensions.concat(['svg']),
-      prepend: '/ember-mdi/',
-      // FIX: an error: Browser failed to connect within 30s. testem.js not loaded?
-      exclude: [
-        'testem'
-      ]
-    },
+    }
   });
 
   app.import('node_modules/@mdi/svg/meta.json', {destDir: ''});
