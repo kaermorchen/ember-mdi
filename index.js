363610565752dafc9871720953c04a0a13a02e67
Stanislav Romanov <kaermorchen@gmail.com>
2019-06-15 06:00:57 +0300

    Small refactoring

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const BroccoliMergeTrees = require('broccoli-merge-trees');
 const writeFile = require('broccoli-file-creator');
-const path = require('path');
-const fs = require('fs');
+const resolve = require('resolve');
 const defaultOptions = {
   icons: null
 };
@@ -11,10 +12,13 @@ const defaultOptions = {
 module.exports = {
   name: require('./package').name,
 
-  included(app) {
+  included() {
     this._super.included.apply(this, arguments);
+    this._ensureFindHost();
 
-    app.import('vendor/ember-mdi/icons.js');
+    const host = this._findHost();
+
+    host.import('vendor/ember-mdi/icons.js');
   },
 
   treeForVendor(vendorTree) {
@@ -24,7 +28,7 @@ module.exports = {
       trees.push(vendorTree);
     }
 
-    const svgsPath = path.join('node_modules', '@mdi', 'svg', 'svg');
+    const svgsPath = path.join(this.resolvePackagePath(path.join('@mdi', 'svg')), 'svg');
     const host = this._findHost();
     const options = Object.assign({}, defaultOptions, host.options[this.name]);
     const list = Array.isArray(options.icons) ? options.icons : fs.readdirSync(svgsPath).map(item => item.slice(0, -4));
@@ -43,4 +47,25 @@ module.exports = {
 
     return new BroccoliMergeTrees(trees);
   },
+
+  resolvePackagePath(packageName) {
+    let host = this._findHost();
+
+    return path.dirname(resolve.sync(`${packageName}/package.json`, { basedir: host.project.root }));
+  },
+
+  _ensureFindHost() {
+    if (!this._findHost) {
+      this._findHost = function findHostShim() {
+        let current = this;
+        let app;
+
+        do {
+          app = current.app || app;
+        } while (current.parent.parent && (current = current.parent));
+
+        return app;
+      };
+    }
+  }
 };
