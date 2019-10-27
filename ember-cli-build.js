88bc7eac471e112f2a9b2d6ec339baa460dd9643
Stanislav Romanov <kaermorchen@gmail.com>
2019-10-27 10:48:01 +0300

    Removed bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,7 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    'ember-cli-bootstrap-4': {
-      js: null
-    }
+
   });
 
   app.import('node_modules/@mdi/svg/meta.json', {destDir: ''});
