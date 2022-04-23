dcae85299df020f158ecefa5f68a811348bf5cd5
Stanislav Romanov <kaermorchen@gmail.com>
2022-04-23 12:01:12 +0300

    Refactored demo app

--- packages/test-app/ember-cli-build.js
+++ packages/test-app/ember-cli-build.js
@@ -8,18 +8,7 @@ module.exports = function (defaults) {
     // Add options here
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  app.import('node_modules/@mdi/svg/meta.json', { destDir: '' });
 
   return require('@embroider/compat').compatBuild(app, Webpack, {
     // staticAddonTestSupportTrees: true,
